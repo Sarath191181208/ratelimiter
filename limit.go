ab0d806bce832f84a554135d411b59bce0ac8bb2
Sarath191181208 <vssarathc04@gmail.com>
2024-04-24 22:39:00 +0530

    + comments

--- limit.go
+++ limit.go
@@ -8,19 +8,25 @@ import (
 )
 
 func ratelimit(next func(writer http.ResponseWriter, request *http.Request)) http.Handler {
-  ratelimiter := rate.NewLimiter(2, 4)
-  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    if !ratelimiter.Allow(){
-      msg := Message{
-        Status: "Too many requests",
-        Body: "The API got too many requests too frequently",
-      }
-      w.WriteHeader(http.StatusTooManyRequests)
-      json.NewEncoder(w).Encode(msg)
-      return
-    } else{
-      next(w, r)
-      return 
-    }
-  })
+	// creating a rate limiter
+	ratelimiter := rate.NewLimiter(2, 4)
+	// handler func
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// checking if raterlimiter is bounced
+		if !ratelimiter.Allow() {
+			// Creating a message
+			msg := Message{
+				Status: "Too many requests",
+				Body:   "The API got too many requests too frequently",
+			}
+			// sending the response
+			w.WriteHeader(http.StatusTooManyRequests)
+			json.NewEncoder(w).Encode(msg)
+			return
+		} else {
+			// running the next func if rate limiter allows
+			next(w, r)
+			return
+		}
+	})
 }
